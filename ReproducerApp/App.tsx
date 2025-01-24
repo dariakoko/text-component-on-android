c6fccdfe1328cf8da28fedaf31d204a338369581
dariakoko <nargle30@gmail.com>
2025-01-24 14:31:20 +0100

    add background color to the text

--- ReproducerApp/App.tsx
+++ ReproducerApp/App.tsx
@@ -6,13 +6,7 @@
  */
 
 import React from 'react';
-import {
-  SafeAreaView,
-  StyleSheet,
-  Text,
-  useColorScheme,
-  View,
-} from 'react-native';
+import {SafeAreaView, Text, useColorScheme, View} from 'react-native';
 
 import {Colors} from 'react-native/Libraries/NewAppScreen';
 
@@ -27,22 +21,27 @@ function App(): React.JSX.Element {
     <SafeAreaView style={backgroundStyle}>
       <View style={{width: 200}}>
         <Text style={{color: 'green'}}>default: </Text>
-        <Text>Some long very very very very very long text</Text>
+        <Text style={{backgroundColor: 'yellow'}}>
+          Some long very very very very very long text
+        </Text>
 
         <Text>-------------</Text>
 
         <Text style={{color: 'green'}}>not selectable: </Text>
-        <Text numberOfLines={1}>
+        <Text numberOfLines={1} style={{backgroundColor: 'yellow'}}>
           Some long very very very very very long text
         </Text>
 
         <Text>-------------</Text>
 
         <Text style={{color: 'red'}}>selectable (issue): </Text>
-
-        <Text selectable={true} numberOfLines={1}>
+        <Text
+          selectable={true}
+          numberOfLines={1}
+          style={{backgroundColor: 'yellow'}}>
           Some long very very very very very long text
         </Text>
+        <View style={{height: 20, backgroundColor: 'pink'}}></View>
       </View>
     </SafeAreaView>
   );
